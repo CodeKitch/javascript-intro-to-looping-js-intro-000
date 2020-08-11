a12a94f1ce55aeec23c3c53a287b0f64974d5168
practical-trainer-5867 <misterjcr@aol.com>
2020-08-11 03:53:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -9,3 +9,11 @@ for (let i = 0; i < 25; i++) {
 return array;
 
 }
+ function whileLoop(n) {
+   let countdown = n;
+
+   while (countdown > 0) {
+     console.log( -- countdown)
+   }
+   return 'done'
+ }
