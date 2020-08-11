061733d4f557a7089dbbbd87366e8481c3895d19
practical-trainer-5867 <misterjcr@aol.com>
2020-08-11 04:01:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,3 +17,16 @@ return array;
    }
    return 'done'
  }
+
+ function doWhileLoop(array) {
+
+   function maybeTrue() {
+     return Math.random() >= 0.5
+   }
+
+   do {
+     array.pop();
+   } while (array.length > 0 && maybeTrue());
+
+   return array;
+ }
